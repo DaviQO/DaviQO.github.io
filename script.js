1834296c565737ddc16da710d10510e0044916ce
DaviQQ <132115852+DaviQO@users.noreply.github.com>
2023-12-19 18:58:37 -0300

    Relogio Digital.

---
+++ script.js
@@ -0,0 +1,14 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+})
